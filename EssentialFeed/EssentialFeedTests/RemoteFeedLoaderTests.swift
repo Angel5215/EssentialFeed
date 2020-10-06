4355791c754eaa4bde4a249b6cc418ad158bf068
Ángel Vázquez <angel_betta@outlook.com>
2020-10-05 22:44:37 -0500

    Request data from URL on `load`

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,19 +8,39 @@
 import XCTest
 
 class RemoteFeedLoader {
-    
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+    }
 }
 
+// Does not make a good match for a singleton. Why does it have to be a singleton?
+// We could very well have more than one HTTPClient.
 class HTTPClient {
+    static let shared = HTTPClient()
+    
+    private init() { }
+    
     var requestedURL: URL?
 }
 
 class RemoteFeedLoaderTests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         _ = RemoteFeedLoader()
         
         // Assert we didn't make a URL Request since that should only happen when `load()` is invoked.
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        // Arrange. "Given a client and a sut"
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+        
+        // Act. "When we invoke sut.load()"
+        sut.load()
+        
+        // Assert. "Then assert that a URL request was initiated in the client"
+        XCTAssertNotNil(client.requestedURL)
     }
 }
