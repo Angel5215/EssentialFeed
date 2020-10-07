edc4f37c4dc0117c0760b89f5dfb58b63cc2ea02
Ángel Vázquez <angel_betta@outlook.com>
2020-10-07 01:14:19 -0500

    Replace optional params with enum with success/failure cases

--- EssentialFeed/EssentialFeed/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/RemoteFeedLoader.swift
@@ -7,9 +7,14 @@
 
 import Foundation
 
+public enum HTTPClientResult {
+    case success(HTTPURLResponse)
+    case failure(Error)
+}
+
 // This can be public because it can be implemented by external modules.
 public protocol HTTPClient {
-    func get(from url: URL, completion: @escaping (Error?, URLResponse?) -> Void)
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
 }
 
 public final class RemoteFeedLoader {
@@ -27,10 +32,11 @@ public final class RemoteFeedLoader {
     }
     
     public func load(completion: @escaping (Error) -> Void) {
-        client.get(from: url) { error, response in
-            if response != nil {
+        client.get(from: url) { result in
+            switch result {
+            case .success:
                 completion(.invalidData)
-            } else {
+            case .failure:
                 completion(.connectivity)
             }
         }
