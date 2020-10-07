9b0b65afc0bd4d0033c508104c0bf5e0366280eb
Ángel Vázquez <angel_betta@outlook.com>
2020-10-06 23:46:03 -0500

    Improve test name

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -16,7 +16,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertNil(client.requestedURL)
     }
     
-    func test_load_requestDataFromURL() {
+    func test_load_requestsDataFromURL() {
         // Arrange. "Given a client and a sut"
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
