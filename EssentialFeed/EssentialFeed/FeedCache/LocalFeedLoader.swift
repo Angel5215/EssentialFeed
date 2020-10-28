42bc9427100ae28e628fee70c9572790494ba193
Ángel Vázquez <angel_betta@outlook.com>
2020-10-27 19:57:09 -0600

    Load command delivers cached images on lees than seven days old cache

--- EssentialFeed/EssentialFeed/FeedCache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/FeedCache/LocalFeedLoader.swift
@@ -32,11 +32,14 @@ public final class LocalFeedLoader {
     }
     
     public func load(completion: @escaping (LoadResult) -> Void) {
-        store.retrieve { error in
-            if let error = error {
+        store.retrieve { result in
+            switch result {
+            case let .failure(error):
                 completion(.failure(error))
-            } else {
+            case .empty:
                 completion(.success([]))
+            case let .found(feed, _):
+                completion(.success(feed.toModels()))
             }
         }
     }
@@ -52,5 +55,11 @@ public final class LocalFeedLoader {
 private extension Array where Element == FeedImage {
     func toLocal() -> [LocalFeedImage] {
         return map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
+    }
+}
+
+private extension Array where Element == LocalFeedImage {
+    func toModels() -> [FeedImage] {
+        return map { FeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
     }
 }
