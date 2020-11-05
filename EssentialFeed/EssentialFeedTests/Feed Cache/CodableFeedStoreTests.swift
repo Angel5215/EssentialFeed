2f518609a66bc829c4c581e2453758ecf6defefb
Ángel Vázquez <angel_betta@outlook.com>
2020-11-04 20:28:07 -0600

    Update capture pattern to use enum associated values  individually as capturing associated values as a tuple is now deprecated

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -133,9 +133,9 @@ class CodableFeedStoreTests: XCTestCase {
             switch (expectedResult, retrievedResult) {
             case (.empty, .empty):
                 break
-            case let (.found(expected), .found(retrieved)):
-                XCTAssertEqual(retrieved.feed, expected.feed, file: file, line: line)
-                XCTAssertEqual(retrieved.timestamp, expected.timestamp, file: file, line: line)
+            case let (.found(feed: expectedFeed, timestamp: expectedTimestamp), .found(feed: retrievedFeed, timestamp: retrievedTimestamp)):
+                XCTAssertEqual(retrievedFeed, expectedFeed, file: file, line: line)
+                XCTAssertEqual(retrievedTimestamp, expectedTimestamp, file: file, line: line)
             default:
                 XCTFail("Expected to retrieve \(expectedResult), got \(retrievedResult) instead", file: file, line: line)
             }
