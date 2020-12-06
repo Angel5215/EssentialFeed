57b6fdca7d8a06d472247c78ce15d1d660f033d4
Ángel Vázquez <angel_betta@outlook.com>
2020-12-05 19:06:39 -0600

    Move ViewModels to separate files

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Ángel Vázquez on 05/12/20.
+//
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Ángel Vázquez on 05/12/20.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
