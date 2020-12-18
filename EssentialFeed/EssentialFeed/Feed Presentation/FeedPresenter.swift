34a7f291ed8fcc2549c17ab1a5653c28b88fdf53
Ángel Vázquez <angel_betta@outlook.com>
2020-12-18 00:24:06 -0600

    Move `FeedErrorViewModel` to a separate file

--- EssentialFeed/EssentialFeed/Feed Presentation/FeedPresenter.swift
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedPresenter.swift
@@ -7,18 +7,6 @@
 
 import Foundation
 
-public struct FeedErrorViewModel {
-    public let message: String?
-    
-    static var noError: FeedErrorViewModel {
-        return FeedErrorViewModel(message: nil)
-    }
-    
-    static func error(message: String) -> FeedErrorViewModel {
-        return FeedErrorViewModel(message: message)
-    }
-}
-
 public struct FeedLoadingViewModel {
     public let isLoading: Bool
 }
