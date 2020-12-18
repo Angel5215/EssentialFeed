a8a778ee05016df076675b6e42a5c67c4e703e1f
Ángel Vázquez <angel_betta@outlook.com>
2020-12-17 22:31:40 -0600

    Does not render load feed error on load

--- EssentialFeed/EssentialFeediOSTests/FeedUI/Helpers/FeedViewController+TestHelpers.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedUI/Helpers/FeedViewController+TestHelpers.swift
@@ -17,6 +17,10 @@ extension FeedViewController {
         return refreshControl?.isRefreshing == true
     }
     
+    var errorMessage: String? {
+        return errorView.message
+    }
+    
     func simulateUserInitiatedFeedReload() {
         refreshControl?.simulatePullToRefresh()
     }
