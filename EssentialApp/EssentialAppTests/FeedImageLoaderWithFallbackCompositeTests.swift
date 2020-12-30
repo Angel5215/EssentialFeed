77a8187df137819dd6492e8061e7b7e82918a980
Ángel Vázquez <angel_betta@outlook.com>
2020-12-30 00:51:20 -0600

    Extract memory leak tracking helper into a shared scope to remove duplication

--- EssentialApp/EssentialAppTests/FeedImageLoaderWithFallbackCompositeTests.swift
+++ EssentialApp/EssentialAppTests/FeedImageLoaderWithFallbackCompositeTests.swift
@@ -121,12 +121,6 @@ class FeedImageLoaderWithFallbackCompositeTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
     
-    private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
-        addTeardownBlock { [weak instance] in
-            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
-        }
-    }
-    
     private func anyURL() -> URL {
         return URL(string: "http://a-url.com")!
     }
