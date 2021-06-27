bf355b8e1651bed1172076dcbf6e568e9ee46409
Ángel Vázquez <angel_betta@outlook.com>
2021-06-26 19:43:46 -0500

    Implement Image Comments UI

---
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/Views/ImageCommentCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCommentCell.swift
+//  EssentialFeediOS
+//
+//  Created by Ángel Vázquez on 26/06/21.
+//
+
+import UIKit
+
+public class ImageCommentCell: UITableViewCell {
+    @IBOutlet private(set) public var messageLabel: UILabel!
+    @IBOutlet private(set) public var usernameLabel: UILabel!
+    @IBOutlet private(set) public var dateLabel: UILabel!
+}
