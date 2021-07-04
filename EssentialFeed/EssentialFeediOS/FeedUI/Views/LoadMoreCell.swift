4973f36dbbb5430220ffde67f13f92a7c151409f
Ángel Vázquez <angel_betta@outlook.com>
2021-07-04 18:01:34 -0500

    Add "Load more" message label

--- EssentialFeed/EssentialFeediOS/FeedUI/Views/LoadMoreCell.swift
+++ EssentialFeed/EssentialFeediOS/FeedUI/Views/LoadMoreCell.swift
@@ -20,6 +20,26 @@ public class LoadMoreCell: UITableViewCell {
         return spinner
     }()
     
+    private lazy var messageLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .tertiaryLabel
+        label.font = .preferredFont(forTextStyle: .footnote)
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.adjustsFontForContentSizeCategory = true
+        contentView.addSubview(label)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            label.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+            label.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            label.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8)
+        ])
+        
+        return label
+    }()
+    
     public var isLoading: Bool {
         get { spinner.isAnimating }
         set {
@@ -29,5 +49,10 @@ public class LoadMoreCell: UITableViewCell {
                 spinner.stopAnimating()
             }
         }
+    }
+    
+    public var message: String? {
+        get { messageLabel.text }
+        set { messageLabel.text = newValue }
     }
 }
