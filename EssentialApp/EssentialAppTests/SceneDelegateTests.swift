b848677bee598f182f8db014e5827506cc7358f6
Ángel Vázquez <angel_betta@outlook.com>
2021-09-29 21:12:02 -0500

    Use UIWindowSpy since in iOS 15+ UIWindow can only be made key in the scene (not the app)
    By using a spy instead of making the window key in the scene, we prevent global side-effects
    in the scene, which could affect other tests and lead to hard-to-debug test failures

--- EssentialApp/EssentialAppTests/SceneDelegateTests.swift
+++ EssentialApp/EssentialAppTests/SceneDelegateTests.swift
@@ -12,14 +12,13 @@ import XCTest
 class SceneDelegateTests: XCTestCase {
     
     func test_configureWindow_setsWindowAsKeyAndVisible() {
-        let window = UIWindow()
+        let window = UIWindowSpy()
         let sut = SceneDelegate()
         sut.window = window
         
         sut.configureWindow()
         
-        XCTAssertTrue(window.isKeyWindow, "Expected window to be the key window")
-        XCTAssertFalse(window.isHidden, "Expected window to be visible")
+        XCTAssertEqual(window.makeKeyAndVisibleCallCount, 1, "Expected window to be the key window")
     }
     
     func test_configureWindow_configuresRootViewController() {
@@ -34,5 +33,13 @@ class SceneDelegateTests: XCTestCase {
         
         XCTAssertNotNil(rootNavigation, "Expected a navigation controller as root, got \(String(describing: root)) instead")
         XCTAssertTrue(topController is ListViewController, "Expected a feed controller as top view controller, got \(String(describing: topController)) instead")
+    }
+    
+    private class UIWindowSpy: UIWindow {
+        private(set) var makeKeyAndVisibleCallCount = 0
+        
+        override func makeKeyAndVisible() {
+            makeKeyAndVisibleCallCount += 1
+        }
     }
 }
